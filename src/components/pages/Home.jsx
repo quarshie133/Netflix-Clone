0d9edc33f8f5051027726f74e1c5eb02a613404a
quarshie133 <wrightsprince@gmail.com>
2022-11-16 11:22:05 +0000

    footer completed

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -9,14 +9,11 @@ function Home() {
       <div className="text-section">
         <h1>Movies</h1>
         <p>
-          <span className="text">
+          <span className="content1">
             Movies move us like nothing else can, whether they're scary, funny,
-            <br />
-            dramatic, romantic or anywhere in-between. So many titles, so much
-            to
-            <br />
-            experience.
-          </span>
+            </span>
+            dramatic, romantic or anywhere in-between. So many titles, so much <span className="content2">to experience.</span>
+        
         </p>
       </div>
       <div className="container-fluid movie-app">
